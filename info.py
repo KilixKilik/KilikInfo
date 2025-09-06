913828f190406562a0c57cd06cd76c1c361744ff
Kerikush <kilikrik69@gmail.com>
2025-09-07 01:34:32 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,23 @@
+from colorama import init, Fore, Style
+import os
+
+init(autoreset=True)
+os.system("title ⚡ KilixKilik Profile")
+
+print(Fore.CYAN + r"""
+    __  ___      __       __  __      
+   /  |/  /___ _/ /______/ /_/ /____ _
+  / /|_/ / __ `/ __/ ___/ __/ __/ __ `/
+ / /  / / /_/ / /_(__  ) /_/ /_/ /_/ / 
+/_/  /_/\__,_/\__/____/\__/\__/\__,_/  
+""")
+
+print(Fore.GREEN + """
+ ✦ Имя: Кирилл (KilixKilik)
+ ✦ Язык: Python 🐍
+ ✦ GitHub: https://github.com/KilixKilik
+ ✦ ОС: Windows 10 💻
+""")
+
+print(Fore.YELLOW + "\n 🚀 Нажми Enter, чтобы выйти...")
+input()
