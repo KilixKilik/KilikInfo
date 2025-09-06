114abec237cc493daf2665c0f0c8ecb0013f7e72
Kerikush <kilikrik69@gmail.com>
2025-09-07 01:41:28 +0800

    Create info.bat

---
+++ info.bat
@@ -0,0 +1,19 @@
+@echo off
+title ⚡ KilixKilik Profile
+cls
+
+echo     __  ___      __       __  __      
+echo    /  ^|/  /___ _/ /______/ /_/ /____ _
+echo   / /^|_/ / __ `/ __/ ___/ __/ __/ __ `/
+echo  / /  / / /_/ / /_(__  ) /_/ /_/ /_/ / 
+echo /_/  /_\`,_,_/\__/____/\__/\__/\`,_,_/  
+echo.
+
+echo  ✦ Имя: Кирилл (KilixKilik)
+echo  ✦ Язык: Python 🐍
+echo  ✦ GitHub: https://github.com/KilixKilik
+echo  ✦ ОС: Windows 10 💻
+echo.
+
+echo  🚀 Нажми любую клавишу...
+pause >nul
