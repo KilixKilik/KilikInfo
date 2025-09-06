68dc0ff960660dc752f9635c5fa93879143983b7
Kerikush <kilikrik69@gmail.com>
2025-09-07 01:37:21 +0800

    Create info.java

---
+++ info.java
@@ -0,0 +1,21 @@
+public class Profile {
+    public static void main(String[] args) throws Exception {
+        System.out.print("\033]0;⚡ KilixKilik Profile\007");
+
+        System.out.println("\033[36m" +
+            "    __  ___      __       __  __      \n" +
+            "   /  |/  /___ _/ /______/ /_/ /____ _\n" +
+            "  / /|_/ / __ `/ __/ ___/ __/ __/ __ `/ \n" +
+            " / /  / / /_/ / /_(__  ) /_/ /_/ /_/ /  \n" +
+            "/_/  /_/\\__,_/\\__/____/\\__/\\__/\\__,_/   \n\033[0m");
+
+        System.out.println("\033[32m" +
+            " ✦ Имя: Кирилл (KilixKilik)\n" +
+            " ✦ Язык: Python 🐍\n" +
+            " ✦ GitHub: https://github.com/KilixKilik\n" +
+            " ✦ ОС: Windows 10 💻\n\033[0m");
+
+        System.out.println("\033[33m 🚀 Нажми Enter, чтобы выйти...\033[0m");
+        System.in.read();
+    }
+}
