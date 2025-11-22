fb0945f895e9d3eb6f4020d026a1f882255627f7
Kerikush <kilikrik69@gmail.com>
2025-11-23 05:15:49 +0800

    Create info.rs

---
+++ info.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Привет из Rust! 🦀");
+    println!("Имя: Кирилл (KilixKilik)");
+    println!("Язык: Rust 🦀");
+    println!("GitHub: https://github.com/KilixKilik");
+    println!("ОС: Windows 10 💻");
+    println!("\n🚀 Нажми Enter, чтобы выйти...");
+}
